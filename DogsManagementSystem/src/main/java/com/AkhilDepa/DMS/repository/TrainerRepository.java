e2d9e8dccfd7cc4e7a079a7908458df003477385
Akhil Reddy Depa <143060945+Akhil2402@users.noreply.github.com>
2024-04-17 18:26:23 -0500

    Add files via upload

---
+++ DogsManagementSystem/src/main/java/com/AkhilDepa/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.AkhilDepa.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.AkhilDepa.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
