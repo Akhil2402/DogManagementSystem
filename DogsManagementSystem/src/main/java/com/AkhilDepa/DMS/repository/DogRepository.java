02529c4c7ecb177931c7370f12b49459d1965c62
Akhil Reddy Depa <143060945+Akhil2402@users.noreply.github.com>
2024-04-17 17:19:39 -0500

    Add files via upload

---
+++ DogsManagementSystem/src/main/java/com/AkhilDepa/DMS/repository/DogRepository.java
@@ -0,0 +1,15 @@
+package com.AkhilDepa.DMS.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.AkhilDepa.DMS.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog, Integer>{
+
+
+	List<Dog> findByName(String name);
+	
+
+}
